1024301fe3358d3426c330557640cc64ca2665e2
youngdeungok-prog <youngdeungok@gmail.com>
2026-04-17 15:06:46 +0900

    chore: update repository name and url in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="A specialized Excel Gantt chart generator for Korean SCM scheduling.",
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url="https://github.com/youngdeungok-prog/my-python-study", # 레파지토리 주소
+    url="https://github.com/youngdeungok-prog/ko-schedule-gantt-chart", # 레파지토리 주소
     packages=find_packages(),                # ko_schedule_gantt 폴더를 자동으로 찾습니다
     install_requires=[                       # 설치 시 자동으로 함께 설치될 라이브러리
         "pandas",
